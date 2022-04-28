b6078d7d9219f8be2a3ea7edeaea5bcaecb0f2d8
ZeriamCloud <zacharydeagle@gmail.com>
2022-04-28 15:26:06 -0600

    first commit (working build)

---
+++ index.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const quote = require('quotesy');
+const sgmail = require('@sendgrid/mail');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const viewsPath = path.join(__dirname, 'templates/views');
+const partialsPath = path.join(__dirname, 'templates/partials');
+const publicPath = path.join(__dirname, 'public');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.set('port', port);
+hbs.registerPartials(partialsPath);
+
+sgmail.setApiKey(
+  process.env.SG_API_KEY
+);
+
+///////////////////////////////////
+app.get('', (req, res) => {
+  res.render('index');
+});
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+getQuote = () => {
+  const newQuote = quote.random();
+  return newQuote;
+};
+
+app.get('/getQuote', (req, res) => {
+  res.send(getQuote());
+});
+
+app.post('/contactMe', (req, res) => {
+  const msg = {
+    to: 'zacharydeagle@gmail.com',
+    from: 'zachdeagleinquiries@protonmail.com',
+    subject: 'portfolio inquiry',
+    text: `from ${req.body.name} at ${req.body.email} saying: ${req.body.body}`,
+  };
+
+  res.send(
+    sgmail.send(msg)
+      .then(() => {
+        console.log('email sent!');
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  );
+
+});
+/////////////////////////////////////////
+
+///////////////////////////////////////
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
---
+++ public/src/app.js
@@ -0,0 +1,95 @@
+const runner = document.querySelectorAll('.item');
+const btn = document.querySelector('.btn');
+const homeArea = document.querySelector('.center');
+const aboutArea = document.querySelector('.center__about');
+const projectArea = document.querySelector('.center__projects');
+const quoteText = document.querySelector('#quoteText');
+const quoteAuthor = document.querySelector('#quoteAuthor');
+const contactArea = document.querySelector('.contact-form');
+
+
+const quoteFunction = function () {
+  fetch('/getQuote').then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        quoteText.textContent = 'Quotesy Error';
+      } else {
+        quoteText.textContent = `"${data.text}"`;
+        quoteAuthor.textContent = `-${data.author}`;
+      }
+    });
+  });
+};
+
+const sendEmail =  function (mail) {
+    fetch('/contactMe', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(mail),
+  }).then((response) => {
+    if(!response.ok) {
+      console.log('error in POST');
+    } else { 
+
+    }
+  })
+};
+
+quoteFunction();
+
+const selector = function (e) {
+  e.preventDefault();
+  // console.log(this);
+  if (!e.target.closest('.item')) {
+    return console.log('Null Sector');
+  } else {
+    const targetItem = e.target.closest('.item').innerHTML;
+
+    if (targetItem === 'Home') {
+      homeArea.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (targetItem === 'About') {
+      aboutArea.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    } else if (targetItem === 'Projects') {
+      projectArea.scrollIntoView({ behavior: 'smooth', block: 'start',});
+    } else if (targetItem === 'Contact Me Now!') {
+      // contactForm.classList.toggle('contact-form__hidden');
+      console.log('Email button');
+      contactArea.scrollIntoView({behavior: 'smooth', block: 'start'});
+      // sendEmail();
+    }
+  }
+};
+
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  const nameData = document.querySelector('.contact-form__submission--name').value;
+  const emailData = document.querySelector('.contact-form__submission--email').value;
+  const textData = document.querySelector('.contact-form__submission--body').value;
+  const sendData = {
+    name : nameData,
+    email : emailData,
+    body : textData
+  };
+
+  if(!nameData || !emailData || !textData) {
+    alert('Please fill out all data fields!')
+  } else {
+    sendEmail(sendData);
+    alert('Email sent succesfully! 😊');
+  }
+})
+
+runner.forEach((item) => {
+  item.addEventListener('click', selector);
+});
+
+const start = () => {
+  console.log('Started!');
+};
+
+start();
